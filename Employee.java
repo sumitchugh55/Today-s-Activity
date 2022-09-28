ce37fd410e3371c4e9ccdbf770c76980d078171d
sumitchugh55 <83135527+sumitchugh55@users.noreply.github.com>
2022-09-28 17:28:05 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,59 @@
+package sumit;
+
+import java.util.*;
+
+
+class Employee
+{
+    int Emp_id;
+     
+    
+     
+    double salary;
+     
+    public Employee(int id,  double salary) 
+    {
+        this.Emp_id = id;
+       
+        this.salary = salary;
+    }
+     
+    public int getEmp_id() 
+    {
+        return Emp_id;
+    }
+     
+    
+     
+   
+   
+    public double getSalary() 
+    {
+        return salary;
+    }
+     
+    
+    public String toString() 
+    {
+        return "Emp_id : "+Emp_id
+               
+                +", Salary : "+salary;
+    }
+
+public static void main(String[] args) {
+List<Employee> employeeList = new ArrayList<Employee>();
+         
+employeeList.add(new Employee(111,  25000.0));
+employeeList.add(new Employee(122, 13500.0));
+employeeList.add(new Employee(133,  18000.0));
+employeeList.add(new Employee(144,  32500.0));
+employeeList.add(new Employee(155, 22700.0));
+
+Optional <Employee> emp = employeeList.stream()
+.sorted(Comparator.comparingDouble(Employee::getSalary).reversed()).skip(1).findFirst();
+
+System.out.println(emp.get());
+
+
+}
+}
